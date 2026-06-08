e7e48285548e61f32d637237105aaa7498e591c6
ElizabethMA76 <bum_76@hotmail.com>
2026-06-07 21:50:40 -0600

    Inicializar proyecto React con Vite

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="hero">
+        <h1>Bienvenido a la Grand Line</h1>
+
+        <p>
+          Explora personajes, tripulaciones, frutas del diablo y todos los
+          secretos de One Piece.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
